6c66b518c8b710867eaf2e7c6fcd8ca80db21f10
macmade <macmade@xs-labs.com>
2019-07-30 21:57:03 +0200

    Cleanup...

--- source/mbr.s
+++ source/mbr.s
@@ -127,9 +127,9 @@ $XEOS.files.stage2              db  "BOOT    BIN"
 ; Strings
 ;-------------------------------------------------------------------------------
 
-$XEOS.boot.stage1.msg.boot      db  "XS: ", @ASCII.NUL
-$XEOS.boot.stage1.msg.error     db  "FAIL", @ASCII.NL, @ASCII.NUL
-$XEOS.boot.stage1.msg.ok        db  "BOOT", @ASCII.NL, @ASCII.NUL
+$XEOS.boot.stage1.msg.boot      db  "XEOS: ", @ASCII.NUL
+$XEOS.boot.stage1.msg.error     db  "FAIL",   @ASCII.NL, @ASCII.NUL
+$XEOS.boot.stage1.msg.ok        db  "OK",     @ASCII.NL, @ASCII.NUL
 
 ;-------------------------------------------------------------------------------
 ; Procedures
